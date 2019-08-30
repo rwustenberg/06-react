bab9df333a8fed1b378d849f057d4de3b1bd9e87
ruthw <rwustenberg@gmail.com>
2019-08-30 02:50:36 -0500

    060502

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,12 +4,31 @@ import '../styles/Thumbnail.css'
 
 class Thumbnail extends React.Component {
 	state = {
-		place: this.props.place}
+		liked: false
+	}
+	likePlace = () => {
+		if(!this.state.liked){
+			this.setState( {
+				liked: true
+			})
+		} else {
+			this.setState( {
+				liked: false
+			})
+		}
+		console.log(this.state.liked)
+	}
+
+
 	render() {
 		return (
 			<div className="Thumbnail">
-				<h3>{this.props.place.name}</h3>
-				<span>{this.props.place.price} {this.props.place.location}</span>
+				<h2>{this.props.place.name}</h2>
+				<h4>{this.props.place.price}</h4>
+				<h3>{this.props.place.location}</h3>
+				<button onClick={this.likePlace} className={this.state.liked ? 'liked': ''}>
+						{this.state.liked ? 'LIKED' : 'LIKE ME'}
+				</button>
 			</div>
 		)
 	}
